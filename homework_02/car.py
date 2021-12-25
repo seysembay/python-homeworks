61269fe28d39d92a3f4d826bcf42daaec152f6d7
darkfever <beket100796@bk.ru>
2021-12-25 16:04:48 +0600

    homework_02 done! resolv -fixed init self.engine

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,18 +1,24 @@
 from homework_02.base import Vehicle
 from homework_02.engine import Engine
+# from base import Vehicle
+# from engine import Engine
 
 
 class Car(Vehicle):
-    engine = ''
 
     def __init__(self, weight, fuel, fuel_consumption):
         super().__init__(weight, fuel, fuel_consumption)
+        self.engine = None
 
     def set_engine(self, value):
         self.engine = value
+
+    def __str__(self):
+        return f'{self.engine}'
 
 
 eng = Engine(10, 6)
 print(eng)
 car_test = Car(20, 100, 3)
 car_test.set_engine(eng)
+print(car_test)
